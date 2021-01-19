2b2196fe09b5d2487d4267bbb0d9777e18f9f8d6
unkel-kp <kp07iam@gmail.com>
2021-01-19 18:33:35 +0530

    First Commit

---
+++ src/test/java/com/kp/jsonserver/jsonserver/JsonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp.jsonserver.jsonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
